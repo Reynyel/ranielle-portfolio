05ba9b1af569d09cf95f58c9325f39ebed5e71dc
Ranielle Lloyd Cruz <raniellecruz16@gmail.com>
2024-08-13 14:21:18 +0800

    add progress bar for skills

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,12 +1,12 @@
 <template>
     <v-container class="project-section" id="projects">
         <div>
-            <p class="text-h5 mb-5">My Projects</p>
+            <p class="text-h3 mb-5">My Projects</p>
         </div>
-        <v-carousel class="fill-height" hide-delimiters>
+        <v-carousel class="fill-height" v-model="model" hide-delimiter-background>
             <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover>
                 <v-row class="fill-height d-flex flex-wrap align-bottom justify-center">
-                    <div class="d-flex align-center justify-center flex-column" style="position: absolute; bottom: 0;">
+                    <div class="d-flex flex-wrap align-center justify-center flex-column pb-5 mb-5" style="position: absolute; bottom: 0;">
                         <div class="carousel-subtitle d-flex align-center justify-center flex-column">
                             <span class="text-h6">
                                 {{ item.title }}
@@ -31,39 +31,41 @@ export default {
                 {
                     title: 'Ros Properties',
                     src: '/images/projects/ros-ui.png',
-                    subtitle: "Developed the frontend interface for a property listings web application",
+                    subtitle: "Developed the frontend interface for a property listings web application. Written in Vue.",
                 },
                 {
                     title: 'Freshcery',
                     src: '/images/projects/freshcery.png',
-                    subtitle: "Developed an e-commerce web app with cart and checkout system.",
+                    subtitle: "Developed an e-commerce web app with cart and checkout system. Powered by PHP and Bootstrap.",
                 },
                 {
                     title: 'Library Management System for Santa Rosa Educational Institute',
                     src: '/images/projects/lms.png',
-                    subtitle: "Created a library system in Java with MySQL for our Capstone 2 project that manages inventory of books, circulation, user data, reports, and backups.",
+                    subtitle: "Created a library system in Java with MySQL for our Capstone 2 project that manages inventory of books, circulation, user data, reports, and backups. Written in Java.",
                 },
                 {
                     title: 'Bayanihan Transpo',
                     src: '/images/projects/transpo2.png',
-                    subtitle: "Developed a vehicle rental web app with distance-based pricing.",
+                    subtitle: "Developed a vehicle rental web app with distance-based pricing. Powered by VueJs and Laravel.",
                 },
                 {
                     title: 'Web Portfolio',
                     src: '/images/projects/portfolio.png',
-                    subtitle: "Developed a web app portfolio do display my projects.",
+                    subtitle: "Developed a web app portfolio do display my projects. Written in Vue.",
                 },
             ],
+            model: 0,
         }
     },
 }
 </script>
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 * {
-    font-family: "Rubik", sans-serif;
+    font-family: "Poppins", sans-serif;
 }
+
 
 
 
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,5 +1,4 @@
 <template>
-
     <v-container class="skills-section" style="min-height: 100vh;" align="center" id="skills">
         <div>
             <p class="text-h3 mb-5">My Skills</p>
@@ -7,40 +6,52 @@
         <div class="d-flex align-center justify-center" align="start" id="container">
             <v-row justify="center">
                 <v-col cols="12" sm="6" md="4" lg="3" class="text-center" v-for="skill in skills" :key="skill.name">
-                    <v-icon large class="ma-4">{{ skill.icon }}</v-icon>
+                    <v-icon large class="ma-4" size="70">{{ skill.icon }}</v-icon>
                     <p class="text-h6 mt-2">{{ skill.name }}</p>
+                    <v-progress-linear :model-value="skill.value" color="blue-grey" height="25"></v-progress-linear>
                 </v-col>
             </v-row>
         </div>
+
     </v-container>
 </template>
+
 <script>
 export default {
     name: "skills",
     data: () => ({
         skills: [
-            { name: "Javascript", icon: "mdi-language-javascript" },
-            { name: "VueJS", icon: "mdi-vuejs" },
-            { name: "HTML5", icon: "mdi-language-html5" },
-            { name: "CSS", icon: "mdi-language-css3" },
-            { name: "Tailwind CSS", icon: "mdi-tailwind" },
-            { name: "PHP", icon: "mdi-language-php" },
-            { name: "Laravel", icon: "mdi-laravel" },
-            { name: "Node.js", icon: "mdi-nodejs" },
-            { name: "Java", icon: "mdi-language-java" },
-            { name: "C#", icon: "mdi-language-csharp" },
-            { name: "Python", icon: "mdi-language-python" },
+            { name: "Javascript", icon: "mdi-language-javascript", value: 65 },
+            { name: "VueJS", icon: "mdi-vuejs", value: 65 },
+            { name: "HTML5", icon: "mdi-language-html5", value: 75 },
+            { name: "CSS", icon: "mdi-language-css3", value: 60 },
+            { name: "Bootstrap", icon: "mdi-bootstrap", value: 70 },
+            { name: "Tailwind CSS", icon: "mdi-tailwind", value: 65 },
+            { name: "PHP", icon: "mdi-language-php", value: 58 },
+            { name: "Laravel", icon: "mdi-laravel", value: 50 },
+            { name: "Node.js", icon: "mdi-nodejs", value: 40 },
+            { name: "Java", icon: "mdi-language-java", value: 80 },
+            { name: "C#", icon: "mdi-language-csharp", value: 60 },
+            { name: "Python", icon: "mdi-language-python", value: 75 },
         ],
+
+
     }),
     methods: {
         zoomIn(event) {
             event.currentTarget.style.transform = 'scale(1.05)';
         },
-
     }
 }
 </script>
+
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+* {
+    font-family: "Poppins", sans-serif;
+}
+
 .skills-section {
     min-height: 100vh; // Full viewport height
     display: flex;
